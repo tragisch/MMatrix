9e96c0d32fc7ff8585f5febe47b7f3256811ad23
tragisch <uwe@roettgermann.de>
2023-04-23 23:46:59 +0200

    add different function to print pretty sparse matrices to console: - braille-form, condensed, scatterplot . (scatterplot with issues)

--- src/include/dm_io.h
+++ src/include/dm_io.h
@@ -4,7 +4,13 @@
 #include "dm_matrix.h"
 #include "dv_vector.h"
 #include "misc.h"
+#include "pbPlots.h"
 #include "sp_matrix.h"
+#include "supportLib.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /*******************************/
 /*     I/O Functions           */
@@ -15,9 +21,15 @@ int dv_read_from_file(DoubleVector *vec, const char *filepath);
 int dv_write_to_file(DoubleVector *vec, const char *filepath);
 
 // print stdout:
-void dm_print(DoubleMatrix *matrix);
-void dv_print(DoubleVector *vec);
-static void dv_print_col(DoubleVector *vec);
-static void dv_print_row(DoubleVector *vec);
+void dm_print(const DoubleMatrix *matrix);
+
+void sp_print(const SparseMatrix *mat);
+void sp_print_braille(const SparseMatrix *mat);
+void sp_print_condensed(SparseMatrix *mat);
+void sp_create_scatterplot(const SparseMatrix *mat, const char *filename);
+
+void dv_print(const DoubleVector *vec);
+static void dv_print_col(const DoubleVector *vec);
+static void dv_print_row(const DoubleVector *vec);
 
 #endif // DM_IO_H
