688efe3bd9d2f61ed08cbe92c5b9f4f9e350fccf
tragisch <uwe@roettgermann.de>
2023-05-29 21:31:24 +0200

    refactor: new dm_utils.c/h contains useful tools

--- src/dm_utils.c
+++ src/dm_utils.c
@@ -9,14 +9,45 @@
  *
  */
 
-
 #include "dm.h"
 #include "dm_io.h"
 #include "dm_modify.h"
+#include <stdio.h>
 
 /*******************************/
 /*  performance measurement    */
 /*******************************/
 
-// function to write doubles to a file as a table with columns from format of matrix
+// write double date to file in a table woth row "name" and colum format
+void write_double_to_file(char *filename, char *name, double data,
+                          matrix_format format) {
+  FILE *fp;
+  fp = fopen(filename, "a");
+  if (fp == NULL) {
+    printf("Error opening file!\n");
+    exit(1);
+  }
+  fprintf(fp, "%s\t%.10lf\t%d\n", name, data, format);
+  fclose(fp);
+}
 
+/*******************************/
+/*         Progress Bar        */
+/*******************************/
+
+// if file to read is very large, print a progress bar:
+void print_progress_bar(size_t progress, size_t total, int barWidth) {
+  float percentage = (float)progress / (float)total;
+  int filledWidth = (int)(percentage * (float)barWidth);
+
+  printf("[");
+  for (int i = 0; i < barWidth; i++) {
+    if (i < filledWidth) {
+      printf("=");
+    } else {
+      printf(" ");
+    }
+  }
+  printf("] %d%%\r", (int)(percentage * 100));
+  fflush(stdout);
+}
---
+++ src/include/dm_utils.h
@@ -0,0 +1,21 @@
+#ifndef DM_UTILS_H
+#define DM_UTILS_H
+
+#include <stdio.h>
+#include <time.h>
+
+// from stackoverflow:
+// https://stackoverflow.com/questions/5248915/execution-time-of-c-program:
+#define CPUTIME(FCALL)                                                         \
+  ({                                                                           \
+    double START = clock();                                                    \
+    FCALL;                                                                     \
+    ((double)clock() - START) / CLOCKS_PER_SEC;                                \
+  })
+
+void write_double_to_file(char *filename, char *name, double data,
+                          matrix_format format);
+
+void print_progress_bar(size_t progress, size_t total, int barWidth);
+
+#endif // DM_UTILS_H
